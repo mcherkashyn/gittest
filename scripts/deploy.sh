09a2a3e333228de0235c01f849cd55e954a1a71d
root <mcherkashinrc@gmail.com>
2022-07-27 18:26:30 +0300

    first commit

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-npm install -g serverless
-npm install --save serverless-finch
-serverless config credentials --provider aws --key env.AWS_KEY --secret env.AWS_SECRET
-npm install
-serverless client deploy --no-confirm
