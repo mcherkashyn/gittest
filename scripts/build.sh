992edb9ff6979c89b8c61882a16405c407cc6f75
mcherkashyn <mcherkashinrc@gmail.com>
2022-08-04 21:10:04 +0300

    Fixed build.sh container tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 docker build -t my-app:version-1.1 ./build
 docker images
 docker run my-app:version-1.1
-docker tag my-app:latest 746279167188.dkr.ecr.eu-north-1.amazonaws.com/my-app:version-1.1
+docker tag my-app:version-1.1 746279167188.dkr.ecr.eu-north-1.amazonaws.com/my-app:version-1.1
 sudo apt install -y awscli
 aws --version
 
