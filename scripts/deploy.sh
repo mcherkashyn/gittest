7bf8247969dde311f4e77645eaf0f7d98937c05e
root <mcherkashinrc@gmail.com>
2022-08-04 15:27:45 +0300

    Remove all unneeded files

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-npm install -g serverless
-npm install --save serverless-finch
-serverless config credentials --provider aws --key AWS_KEY --secret AWS_SECRET
-npm install
-serverless client deploy --no-confirm
