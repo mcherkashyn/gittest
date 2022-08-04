224c465eb5c0497de7a87b315a34685e769f1908
mcherkashyn <mcherkashinrc@gmail.com>
2022-08-04 21:02:07 +0300

    Added container tag changing

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,7 @@
 docker build -t my-app:version-1.1 ./build
 docker images
 docker run my-app:version-1.1
+docker tag my-app:latest 746279167188.dkr.ecr.eu-north-1.amazonaws.com/my-app:version-1.1
 sudo apt install -y awscli
 aws --version
 
-#export AWS_ACCESS_KEY_ID=AWS_KEY
-#export AWS_SECRET_ACCESS_KEY=AWS_SECRET
-#export AWS_DEFAULT_REGION=us-east-1
-#aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 746279167188.dkr.ecr.eu-north-1.amazonaws.com
