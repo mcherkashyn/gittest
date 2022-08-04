badd3d08e785d59b3cfb329466fb9c95aa050f1f
mcherkashyn <mcherkashinrc@gmail.com>
2022-08-04 20:02:06 +0300

    Added AWS CLI command

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,3 +6,4 @@ aws --version
 export AWS_ACCESS_KEY_ID=AWS_KEY
 export AWS_SECRET_ACCESS_KEY=AWS_SECRET
 export AWS_DEFAULT_REGION=us-east-1
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 746279167188.dkr.ecr.eu-north-1.amazonaws.com
